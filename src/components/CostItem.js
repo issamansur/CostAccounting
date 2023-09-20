3d8bd7dcf4e8b41c5e9fbdf0dcb63598a4bff28f
User#None <77580844+issamansur@users.noreply.github.com>
2023-09-20 11:27:27 +0000

    2.19 use Composition(props.children) - div to Card

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,16 +1,19 @@
+import "./CostItem.css";
+
+import Card from "./Card";
+
 import CostDate from "./CostDate";
-import "./CostItem.css"
 
 function CostItem(props) {
 
     return (
-    <div className="cost-item">
+    <Card className="cost-item">
         <CostDate date={props.date}/>
         <div className="cost-item__description">
             <h2>{props.description}</h2>
             <div className="cost-item__price">${props.amount}</div>
         </div>
-    </div>
+    </Card>
     );
 }
 
