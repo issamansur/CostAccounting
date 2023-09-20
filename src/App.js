c2a87dae7f16b1a54a7b9dfed310a1ed28b02c40
User#None <77580844+issamansur@users.noreply.github.com>
2023-09-20 10:47:52 +0000

    2.17-18 Putting Costitem's in new Costs component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CostItem from "./components/CostItem";
+import Costs from "./components/Costs"
 
 function App() {
   const costs = [
@@ -22,9 +22,7 @@ function App() {
   return (
     <div className="lol">
       <h1>Реакт</h1>
-      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
-      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
-      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>
+      <Costs costs={costs} />
     </div>
   );
 }
