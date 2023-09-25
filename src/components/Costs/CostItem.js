8a374ea5f5e59bfd881fb8d7210354a6fcb5ddeb
User#None <77580844+issamansur@users.noreply.github.com>
2023-09-25 15:56:56 +0000

    3.17 remove unused button with handler n useState

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,28 +1,16 @@
-import { useState } from "react";
-
 import "./CostItem.css";
 
 import Card from "../UI/Card";
 import CostDate from "./CostDate";
 
 const CostItem = (props) => {
-    const [description, setDescriprion] = 
-        useState(props.description);
-
-    const changeDescriptionHandler = () => {
-        setDescriprion("New text");
-    }
-
     return (
         <Card className="cost-item">
             <CostDate date={props.date}/>
             <div className="cost-item__description">
-                <h2>{description}</h2>
+                <h2>{props.description}</h2>
                 <div className="cost-item__price">${props.amount}</div>
             </div>
-            <button onClick={changeDescriptionHandler}>
-                Изменить описание
-            </button>
         </Card>
     );
 };
