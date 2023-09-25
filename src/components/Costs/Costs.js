d506b3bbd9041ad387ddcce2cf498c2ef72f19ca
User#None <77580844+issamansur@users.noreply.github.com>
2023-09-25 16:06:52 +0000

    3.2 add 'map()' to display 'CostItem's

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -16,11 +16,20 @@ const Costs = (props) => {
     return (
         <div>
             <Card className="costs">
-                <CostsFilter year={inputYear} onChangeYear={changeYearHandler} />
+                <CostsFilter 
+                    year={inputYear} 
+                    onChangeYear={changeYearHandler}
+                />
                 
-                <CostItem date={props.costs[0].date} description={props.costs[0].description} amount={props.costs[0].amount}/>
-                <CostItem date={props.costs[1].date} description={props.costs[1].description} amount={props.costs[1].amount}/>
-                <CostItem date={props.costs[2].date} description={props.costs[2].description} amount={props.costs[2].amount}/>
+                {props.costs.map(
+                    (cost) => (
+                        <CostItem 
+                            description={cost.description}
+                            amount={cost.amount}
+                            date={cost.date}
+                        />
+                    )
+                )}
             </Card>
         </div>
     );
