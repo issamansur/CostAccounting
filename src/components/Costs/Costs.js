6064ca5a4878a689795aaabcc049a64f27367af2
User#None <77580844+issamansur@users.noreply.github.com>
2023-09-25 13:31:34 +0000

    3.16 Add filter by rise of the state + 2 link <->

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,14 +1,29 @@
+import React, { useState } from "react";
+
 import "./Costs.css";
 
+import CostsFilter from "./CostsFilter";
 import Card from "../UI/Card";
 import CostItem from "./CostItem";
 
-const Costs = (props) => (
-    <Card className="costs">
-        <CostItem date={props.costs[0].date} description={props.costs[0].description} amount={props.costs[0].amount}/>
-        <CostItem date={props.costs[1].date} description={props.costs[1].description} amount={props.costs[1].amount}/>
-        <CostItem date={props.costs[2].date} description={props.costs[2].description} amount={props.costs[2].amount}/>
-    </Card>
-);
+const Costs = (props) => {
+    const [inputYear, setInputYear] = useState('2020');
+
+    const changeYearHandler = (year) => {
+        setInputYear(year);
+    };
+    
+    return (
+        <div>
+            <Card className="costs">
+                <CostsFilter year={inputYear} onChangeYear={changeYearHandler} />
+                
+                <CostItem date={props.costs[0].date} description={props.costs[0].description} amount={props.costs[0].amount}/>
+                <CostItem date={props.costs[1].date} description={props.costs[1].description} amount={props.costs[1].amount}/>
+                <CostItem date={props.costs[2].date} description={props.costs[2].description} amount={props.costs[2].amount}/>
+            </Card>
+        </div>
+    );
+};
 
 export default Costs;
